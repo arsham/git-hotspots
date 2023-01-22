3f04def68b3b0d0494947bdee3cdf15222c8e739
Arsham Shirvani <arshamshirvani@gmail.com>
2023-01-22 01:34:47 +0000

    feat(insight): add a crate to intoregate repos
    For now we invoke the git binary until we figure out how to use the git2
    crate for this functionality.

---
+++ insight/src/lib.rs
@@ -0,0 +1,132 @@
+use std::process::Command;
+use std::{io, str};
+
+use grep_matcher::{Captures, Matcher};
+use grep_regex::RegexMatcher;
+use grep_searcher::sinks::UTF8;
+use grep_searcher::Searcher;
+use thiserror::Error as TError;
+
+/// Inspector interrogates the git repository for history of functions and methods.
+pub struct Inspector {
+    matcher: RegexMatcher,
+    path: String,
+}
+
+/// Error enumerates all errors for this application.
+#[derive(TError, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    IO(#[from] io::Error),
+
+    #[error(transparent)]
+    Grep(#[from] grep_regex::Error),
+
+    #[error(transparent)]
+    UTF8(#[from] str::Utf8Error),
+
+    #[error("Not a git directory")]
+    NotGitRepo,
+}
+
+impl Inspector {
+    /// Returns an error if the path is not a valid repository.
+    pub fn new(path: &str) -> Result<Self, Error> {
+        let output = Command::new("git")
+            .args(["rev-parse", "--is-inside-work-tree"])
+            .current_dir(path)
+            .output()?;
+        if output.status.success() {
+            Ok(Inspector {
+                matcher: RegexMatcher::new(r"^commit (.{40})")?,
+                path: String::from(path),
+            })
+        } else {
+            Err(Error::NotGitRepo)
+        }
+    }
+
+    /// Returns the commits that the function by func_name appears for the filename from beginning
+    /// of the repository.
+    pub fn function_history(&self, filename: &str, func_name: &str) -> Result<Vec<String>, Error> {
+        let input = format!(":{func_name}:{filename}");
+        let output = Command::new("git")
+            .args(["log", "-L", &input])
+            .current_dir(&self.path)
+            .output()?;
+        self.commits(str::from_utf8(&output.stdout)?)
+    }
+
+    fn commits(&self, input: &str) -> Result<Vec<String>, Error> {
+        let mut matches: Vec<String> = vec![];
+        Searcher::new().search_slice(
+            &self.matcher,
+            input.as_bytes(),
+            UTF8(|_, line| {
+                let mut caps = self.matcher.new_captures()?;
+                match self.matcher.captures(line.as_bytes(), &mut caps) {
+                    Ok(true) => {
+                        matches.push(line[caps.get(1).unwrap()].to_owned());
+                        Ok(true)
+                    },
+                    _ => Ok(false),
+                }
+            }),
+        )?;
+        Ok(matches)
+    }
+}
+
+#[cfg(test)]
+mod commits {
+    use itertools::assert_equal;
+    use speculoos::prelude::*;
+
+    use super::*;
+
+    fn new_inspector() -> Inspector {
+        let (dir, _) = utilities::repo_init();
+        let path = dir.path().as_os_str().to_string_lossy().to_string();
+        let inspector = Inspector::new(path.as_str()).unwrap();
+        inspector
+    }
+
+    #[test]
+    fn empty_input() -> Result<(), Box<dyn std::error::Error>> {
+        let inspector = new_inspector();
+        let res = inspector.commits("")?;
+        assert_that!(res).is_empty();
+        Ok(())
+    }
+
+    #[test]
+    fn no_commit_in_input() -> Result<(), Box<dyn std::error::Error>> {
+        let inspector = new_inspector();
+        let input = "something\nsomething\ncommit 1234\nnooo";
+        let res = inspector.commits(input)?;
+        assert_that!(res).is_empty();
+        Ok(())
+    }
+
+    #[test]
+    fn commit_in_input() -> Result<(), Box<dyn std::error::Error>> {
+        let inspector = new_inspector();
+        let hash = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        let input = format!("commit {hash}");
+        let res = inspector.commits(&input)?;
+        assert_that!(res).has_length(1);
+        assert_that!(res.get(0).unwrap()).is_equal_to(&hash.to_owned());
+        Ok(())
+    }
+    #[test]
+    fn finds_commits() -> Result<(), Box<dyn std::error::Error>> {
+        let inspector = new_inspector();
+        let hash1 = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        let hash2 = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb";
+        let hash3 = "cccccccccccccccccccccccccccccccccccccccc";
+        let input = format!("commit {hash1}\nnocommit {hash2}\ncommit {hash3}\n");
+        let res = inspector.commits(&input)?;
+        assert_equal(res, vec![hash1, hash3]);
+        Ok(())
+    }
+}
--- utilities/src/lib.rs
+++ utilities/src/lib.rs
@@ -1,4 +1,8 @@
-use std::{fs::File, io, path::Path};
+use std::fs::File;
+use std::io;
+use std::path::Path;
+
+use git2::{Repository, RepositoryInitOptions};
 use tempfile::TempDir;
 
 pub fn create_files(path: &TempDir, files: Vec<&str>) -> io::Result<()> {
@@ -10,4 +14,26 @@ pub fn create_files(path: &TempDir, files: Vec<&str>) -> io::Result<()> {
         File::create(filename)?;
     }
     Ok(())
+}
+
+// Some of these helpers are borrowed from the git2 code.
+
+pub fn repo_init() -> (TempDir, Repository) {
+    let td = TempDir::new().unwrap();
+    let mut opts = RepositoryInitOptions::new();
+    opts.initial_head("master");
+    let repo = Repository::init_opts(td.path(), &opts).unwrap();
+    {
+        let mut config = repo.config().unwrap();
+        config.set_str("user.name", "name").unwrap();
+        config.set_str("user.email", "email").unwrap();
+        let mut index = repo.index().unwrap();
+        let id = index.write_tree().unwrap();
+
+        let tree = repo.find_tree(id).unwrap();
+        let sig = repo.signature().unwrap();
+        repo.commit(Some("HEAD"), &sig, &sig, "initial\n\nbody", &tree, &[])
+            .unwrap();
+    }
+    (td, repo)
 }
